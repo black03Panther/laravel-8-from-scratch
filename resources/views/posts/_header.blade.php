ec0f79a814c9d5c38560068caa597698715e422a
black03Panther <henning.jeschke@urs-os.de>
2023-05-24 22:30:13 +0200

    EP40

--- resources/views/posts/_header.blade.php
+++ resources/views/posts/_header.blade.php
@@ -8,28 +8,7 @@
         <!--  Category -->
         <div class="relative lg:inline-flex items-center bg-gray-100 rounded-xl">
 
-        <x-dropdown>
-            <x-slot name="trigger">
-            <button  class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left flex lg:inline-flex"
-                >
-                 {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories'}}
-
-                <x-down-arrow class="absolute pointer-events-none"style="right: 12px;"/>
-                </button>
-            </x-slot>
-
-            <x-dropdown-item href="/" :active="request()->routeIs('home')">All</x-dropdown-item>
-            
-            @foreach ($categories as $category)
-  
-                <x-dropdown-item 
-                href="/categories/{{ $category->slug }}"
-                :active="isset($currentCategory) && $currentCategory->is($category)"
-                >
-                {{ ucwords($category->name) }}
-            </x-dropdown-item>    
-            @endforeach
-            </x-dropdown>
+        <x-category-dropdown />
         </div>
 
         <!-- Other Filters -->
